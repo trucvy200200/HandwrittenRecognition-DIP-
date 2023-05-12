990c138f6e96b0b968d29f3acd06ef7f7064bde4
trucvy200200 <toan9400@gmail.com>
2023-05-12 10:09:35 +0700

    Change UI

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,25 +1,71 @@
 import numpy as np
 import streamlit as st
+import streamlit.components.v1 as components
 from PIL import Image
 from io import BytesIO
 from main import predict
 from his_horizontal import Extract
-import base64
 
 ex = Extract()
 
 st.set_page_config(layout="wide", page_title="Text Extracter")
 
-names, group = st.columns(2)
-with st.container():
-    with names.expander("Members"):
-        st.write("## Nguyễn Thị Tuyết Mai - 20110381")
-        st.write("## Vũ Hoàng Trúc Vy - 20110415")
-        st.write("## Nguyễn Văn Hơn - 20110371")
+def hide_footer():
+  # hide footer
+  hide_footer_style = """
+  <style>
+  footer {visibility: hidden;}
+  div
+  {
+      font-size: 14px
+  }
+  div.sticky {
+  position:fixed !important;
+  top: 0 !important;
+  background-color: black !important;
+  margin-top: 2.875rem !important;
+  font-size: 20px;
+  z-index: 100;
+  align-items: center;
+  }
 
-    group.write("Group 3")
+  div.sticky-right {
+  position:fixed !important;
+  top: 0 !important;
+  left: 50vh;
+  background-color: black !important;
+  margin-top: 2.875rem !important;
+  margin-left: 50vh !important;
+  font-size: 20px;
+  z-index: 100;
+  align-items: center;
+  }
+
+  div.sticky .column {
+  flex: 1;
+  padding: 10px;
+  }
+
+  </style>
+  """
+  st.markdown(hide_footer_style, unsafe_allow_html=True)
+
+st.write("""<div class="sticky"> <div><details> <summary>Team Members</summary>
+<ul class="team-members">
+<li>Nguyễn Thị Tuyết Mai - 20110381</li> 
+<li>Vũ Hoàng Trúc Vy - 20110415</li>
+<li>Nguyễn Văn Hơn - 20110371</li>
+</ul> </details> </div>
+</div>""", unsafe_allow_html=True)
+
+st.write("""<div class="sticky-right"> <div><details> <summary>Group 3</summary>
+<ul class="team-content">
+<li>HANDWRITTEN TEXT RECOGNITION USING IMAGE PROCESSING AND PATTERN RECOGNITION TECHNIQUES</li> 
+</ul> </details> </div>
+</div>""", unsafe_allow_html=True)
 
 
+hide_footer()
 
 st.write("## Extract text from your image")
 st.write(
@@ -52,28 +98,12 @@ def fix_image(upload):
 
     extracted_content = predict(cropped)
 
-    # extracted_content = "\n".join(extracted_content)
-    # extracted_content = extracted_content
-
     col2.write("Extracted Image :wrench:")
     col2.image(cropped, caption=extracted_content)
-    # st.image(images_on_page, width=100, caption=indices_on_page)
-    # col2.write(extracted_content)
+    
     st.sidebar.markdown("\n")
 
 
-# hide footer
-hide_footer_style = """
-<style>
-footer {visibility: hidden;}
-div
-{
-    font-size: 14px
-}
-</style>
-"""
-
-st.markdown(hide_footer_style, unsafe_allow_html=True)
 
 col1, col2 = st.columns(2)
 
@@ -81,6 +111,9 @@ my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpe
 
 
 if my_upload is not None:
-    fix_image(upload=my_upload)
+    try:
+        fix_image(upload=my_upload)
+    except:
+        st.warning('Please upload a valid image', icon="⚠️")
 else:
     fix_image("./images/cropImage-0.png")
